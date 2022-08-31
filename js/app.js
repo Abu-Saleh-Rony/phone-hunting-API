87b0c430e17fe20059d1a63f16ba2b7893ac5f37
abusalehrony <abusalehrony12@gmail.com>
2022-08-31 16:37:10 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const loadphone = (searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayphones(data.data));
+}
+
+const displayphones = phones => {
+    const phonesContainer = document.getElementById('phone-container');
+    phonesContainer.innerText = '';
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-5">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${phone.phone_name}</h5>
+                            <p class="card-text">A mobile phone is one kind of portable telephone. A great discovery of science. We get and send news, play game, time, calculator, etc. everything from the phone. Now the world is getting like a village which can be called a global village.</p>
+                        </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+
+    })
+
+
+}
+
+document.getElementById('btn-search').addEventListener('click', function () {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadphone(searchText);
+})
+
+loadphone();
